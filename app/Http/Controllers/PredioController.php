c6cdb5ebf53a77998af895f824e4baf183cceccb
EliotVonEcklie <eliotvonecklie@gmail.com>
2023-12-04 17:13:16 -0500

    added predio model

---
+++ app/Http/Controllers/PredioController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Predio;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StorePredioRequest;
+use App\Http\Requests\UpdatePredioRequest;
+
+class PredioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePredioRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Predio $predio)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Predio $predio)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePredioRequest $request, Predio $predio)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Predio $predio)
+    {
+        //
+    }
+}
