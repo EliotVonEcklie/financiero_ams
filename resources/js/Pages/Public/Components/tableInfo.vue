78bbbd47019e58dd85771b511a08161efa425e57
MrNavySeal <dsparrado197023@gmail.com>
2024-01-12 08:11:09 -0500

    dev

--- resources/js/Pages/Public/Components/tableInfo.vue
+++ resources/js/Pages/Public/Components/tableInfo.vue
@@ -43,7 +43,7 @@ function search(evt) {
                     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                             <tr class="text-center font-semibold py-3 text-black bg-gray-100">
-                                <td colspan="8">Resultados de Búsqueda</td>
+                                <td colspan="8">Resultados de Búsqueda: {{ predios.length }} </td>
                             </tr>
                             <tr>
                                 <th scope="col" class="px-6 py-3">
