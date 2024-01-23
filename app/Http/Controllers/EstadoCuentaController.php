c3b7a4a5448e4baaf92b08ded7df530a944ba797
EliotVonEcklie <eliotvonecklie@gmail.com>
2024-01-23 10:39:49 -0500

    Dev factura predial

--- app/Http/Controllers/EstadoCuentaController.php
+++ app/Http/Controllers/EstadoCuentaController.php
@@ -9,22 +9,6 @@ use Illuminate\Http\Request;
 class EstadoCuentaController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
@@ -45,29 +29,5 @@ class EstadoCuentaController extends Controller
         return Pdf::loadView('pdf.estado_cuenta', [
             'estadoCuenta' => $estadoCuenta
         ])->stream($estadoCuenta->id . '_' . now()->format('YmdHis') . '_estado-cuenta.pdf');
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(EstadoCuenta $estadoCuenta)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, EstadoCuenta $estadoCuenta)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(EstadoCuenta $estadoCuenta)
-    {
-        //
     }
 }
