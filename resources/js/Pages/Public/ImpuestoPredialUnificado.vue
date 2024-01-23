dc57325a4a15122dfd51458278944b31b59d3392
EliotVonEcklie <eliotvonecklie@gmail.com>
2024-01-23 15:06:44 -0500

    Fixes

--- resources/js/Pages/Public/ImpuestoPredialUnificado.vue
+++ resources/js/Pages/Public/ImpuestoPredialUnificado.vue
@@ -125,10 +125,9 @@
                         <td class="border-b border-gray-300 text-bold text-black  bg-white dark:bg-gray-800 dark:border-gray-700 dark:text-white">
                             <p class="px-5 py-2 font-bold">Dirección:</p>
                             <p class="px-5 py-2 text-black dark:text-gray-400">{{ predio.direccion }}</p>
-                            <p class="px-5 py-2 text-black dark:text-gray-400">{{ predio.direccion }}</p>
                         </td>
                         <td class="border-b border-gray-300 text-bold text-black  bg-white dark:bg-gray-800 dark:border-gray-700 dark:text-white">
-                            <p class="px-5 py-2 font-bold">Avaluo vigente:</p>
+                            <p class="px-5 py-2 font-bold">Avalúo vigente:</p>
                             <p class="px-5 py-2 text-black dark:text-gray-400">{{ formatNumber(predio.valor_avaluo) }}</p>
                         </td>
                     </tr>
