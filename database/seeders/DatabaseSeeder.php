3abfb56a229b83acdcbfe9e2d6c101c2cba845ed
EliotVonEcklie <eliotvonecklie@gmail.com>
2024-01-25 14:59:10 -0500

    Add import intereses

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,18 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        if (tenant() === null) {
-            \App\Models\Tenant::firstOrCreate([
-                'id' => 'rosalia'
-            ], [
-                'tenancy_db_name' => 'rosalia'
-            ])->domains()->firstOrCreate([
-                'domain' => 'rosalia'
-            ]);
-
-            return;
-        }
-
         \App\Models\UnidadMonetaria::create([
             'nombre' => 'UVT'
         ]);
@@ -58,6 +48,17 @@ class DatabaseSeeder extends Seeder
             \App\Models\PredioEstrato::create([
                 'estrato' => $i
             ]);
+        }
+
+        if (Storage::disk('data')->exists('interes.json')) {
+            foreach(json_decode(Storage::disk('data')->get('interes.json'))->interes as $interes) {
+                \App\Models\Interes::create([
+                    'vigencia' => $interes->vigencia,
+                    'mes' => $interes->mes,
+                    'moratorio' => $interes->moratorio,
+                    'corriente' => $interes->corriente
+                ]);
+            }
         }
     }
 }
