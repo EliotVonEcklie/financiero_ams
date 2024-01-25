ddb976ec14d6348b9000ff2a0cbcd8aace413169
MrNavySeal <dsparrado197023@gmail.com>
2024-01-25 16:23:41 -0500

    dev

--- resources/js/Pages/Public/Layout.vue
+++ resources/js/Pages/Public/Layout.vue
@@ -4,7 +4,7 @@
 
         <nav class="bg-white border-gray-200 dark:bg-gray-900">
             <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-3">
-                <Link :href="route('public.index')" class="flex items-center space-x-3 rtl:space-x-reverse">
+                <Link :href="tenant.pagina" class="flex items-center space-x-3 rtl:space-x-reverse">
                     <img :src="img" class="lg:h-20 md:h-40" alt="Logo Entidad" />
                     <div class="flex flex-col">
                         <span class="self-center lg:text-2xl font-semibold whitespace-nowrap md:text-3xl dark:text-white">{{ tenant.nombre.toUpperCase() }} - META</span>
@@ -27,7 +27,7 @@
         <nav :class="{'translate-x-0 ':mobileNav, 'translate-x-full ':mobileNav==false}" class="fixed z-50 top-0 transition-all h-full w-screen ">
             <div class=" h-full bg-white overflow-y-auto dark:bg-gray-700" >
                 <div class="flex justify-between items-center mt-4 p-4">
-                    <Link :href="route('public.index')" class="flex items-center space-x-3 rtl:space-x-reverse">
+                    <Link :href="tenant.pagina" class="flex items-center space-x-3 rtl:space-x-reverse">
                         <img :src="img" class="h-20 md:h-40" alt="Logo Entidad" />
                         <div class="flex flex-col">
                             <span class="self-center text-2xl font-semibold whitespace-nowrap md:text-3xl dark:text-white">{{ tenant.nombre.toUpperCase() }} - META</span>
@@ -125,7 +125,7 @@
     <footer class="bg-gray-100 dark:bg-gray-900 min-h-1/2">
         <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8 lg:flex lg:items-center justify-between lg:flex-row md:flex-col">
             <div class="mb-6">
-                <Link :href="route('public.index')" class="flex items-center md:justify-center">
+                <Link :href="tenant.pagina" class="flex items-center md:justify-center">
                     <img :src="img" class="lg:h-8 me-3 md:h-24" alt="Logo Entidad" />
                     <div class="flex flex-col">
                         <span class="self-center lg:text-base font-semibold md:text-2xl whitespace-nowrap dark:text-white">{{ tenant.nombre.toUpperCase() }} - META</span>
@@ -140,7 +140,7 @@
                         <li class="mb-4">
                             <Link href="#" class="hover:underline">Preguntas frecuentes</Link>
                         </li>
-                        <li>
+                        <li class="mb-4">
                             <Link href="#" class="hover:underline">Manuales de sistema</Link>
                         </li>
                     </ul>
@@ -150,15 +150,21 @@
                     <ul class="lg:text-base md:text-2xl text-gray-500 dark:text-gray-400 font-medium">
                         <li class="mb-4">
                             <div class="flex space-x-2 items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z"/></svg>
+                                <svg class="lg:w-3 lg:h-3 md:w-7 md:h-7" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 512 512"><path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z"/></svg>
                                 <span>{{ tenant.telefono }}</span>
                             </div>
                         </li>
-                        <li>
+                        <li class="mb-4">
                             <div class="flex space-x-2 items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg>
+                                <svg class="lg:w-3 lg:h-3 md:w-7 md:h-7" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 512 512"><path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg>
                                 <span>{{ tenant.correo }}</span>
                             </div>
+                        </li>
+                        <li class="mb-4">
+                            <Link :href="tenant.pagina" class="flex space-x-2 items-center">
+                                <svg class="lg:w-3 lg:h-3 md:w-7 md:h-7" xmlns="http://www.w3.org/2000/svg"   viewBox="0 0 512 512"><path d="M352 256c0 22.2-1.2 43.6-3.3 64H163.3c-2.2-20.4-3.3-41.8-3.3-64s1.2-43.6 3.3-64H348.7c2.2 20.4 3.3 41.8 3.3 64zm28.8-64H503.9c5.3 20.5 8.1 41.9 8.1 64s-2.8 43.5-8.1 64H380.8c2.1-20.6 3.2-42 3.2-64s-1.1-43.4-3.2-64zm112.6-32H376.7c-10-63.9-29.8-117.4-55.3-151.6c78.3 20.7 142 77.5 171.9 151.6zm-149.1 0H167.7c6.1-36.4 15.5-68.6 27-94.7c10.5-23.6 22.2-40.7 33.5-51.5C239.4 3.2 248.7 0 256 0s16.6 3.2 27.8 13.8c11.3 10.8 23 27.9 33.5 51.5c11.6 26 20.9 58.2 27 94.7zm-209 0H18.6C48.6 85.9 112.2 29.1 190.6 8.4C165.1 42.6 145.3 96.1 135.3 160zM8.1 192H131.2c-2.1 20.6-3.2 42-3.2 64s1.1 43.4 3.2 64H8.1C2.8 299.5 0 278.1 0 256s2.8-43.5 8.1-64zM194.7 446.6c-11.6-26-20.9-58.2-27-94.6H344.3c-6.1 36.4-15.5 68.6-27 94.6c-10.5 23.6-22.2 40.7-33.5 51.5C272.6 508.8 263.3 512 256 512s-16.6-3.2-27.8-13.8c-11.3-10.8-23-27.9-33.5-51.5zM135.3 352c10 63.9 29.8 117.4 55.3 151.6C112.2 482.9 48.6 426.1 18.6 352H135.3zm358.1 0c-30 74.1-93.6 130.9-171.9 151.6c25.5-34.2 45.2-87.7 55.3-151.6H493.4z"/></svg>
+                                <span>{{ tenant.pagina }}</span>
+                            </Link>
                         </li>
                     </ul>
                 </div>
@@ -167,7 +173,7 @@
                     <ul class="lg:text-base md:text-2xl text-gray-500 dark:text-gray-400 font-medium">
                         <li class="mb-4">
                             <div class="flex space-x-2 items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" height="16" width="12" viewBox="0 0 384 512"><path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/></svg>
+                                <svg class="lg:w-3 lg:h-3 md:w-7 md:h-7" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 384 512"><path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/></svg>
                                 <span>{{ tenant.direccion }}</span>
                             </div>
                         </li>
