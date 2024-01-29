c67394aabfa3d47c6d5d1e9e37ca1cabf5c25b54
EliotVonEcklie <eliotvonecklie@gmail.com>
2024-01-29 09:44:30 -0500

    Fix tables

--- resources/js/Pages/EstadoCuentas/Create.vue
+++ resources/js/Pages/EstadoCuentas/Create.vue
@@ -150,21 +150,21 @@ function create() {
                     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                         <thead class="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
                             <tr>
-                                <th scope="col" class="p-4">
+                                <th scope="col" class="p-4 rounded-s-lg">
                                     <div class="flex items-center">
                                         <input id="checkbox-all-search" type="checkbox" v-model="allSelected" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
                                         <label for="checkbox-all-search" class="sr-only">checkbox</label>
                                     </div>
                                 </th>
 
-                                <th scope="col" class="px-6 py-3 rounded-s-lg">
+                                <th scope="col" class="px-6 py-3">
                                     Vigencia
                                 </th>
 
-                                <th scope="col" class="px-6 py-3 rounded-e-lg">
+                                <th scope="col" class="px-6 py-3">
                                     Avalúo
                                 </th>
-                                <th scope="col" class="px-6 py-3 rounded-e-lg">
+                                <th scope="col" class="px-6 py-3">
                                     Tasa
                                 </th>
 
@@ -199,7 +199,7 @@ function create() {
                                     Alumbrado
                                 </th>
 
-                                <th scope="col" class="px-6 py-3 rounded-e-lg border-l-2 border-l-gray-200 dark:border-l-gray-500">
+                                <th scope="col" class="px-6 py-3 border-l-2 border-l-gray-200 dark:border-l-gray-500">
                                     Liquidación
                                 </th>
                                 <th scope="col" class="px-6 py-3 rounded-e-lg">
