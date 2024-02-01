633ffe2a2e60cf7b26ae1b435d4edce7f1fb298d
EliotVonEcklie <eliotvonecklie@gmail.com>
2024-02-01 14:57:45 -0500

    Fix pdf buttons

--- resources/js/Pages/FacturaPredials/Index.vue
+++ resources/js/Pages/FacturaPredials/Index.vue
@@ -84,11 +84,11 @@ function openPdf(evt) {
                     <template #id="{ element }">{{ element.id }}</template>
 
                     <template #row="{ element }">
-                        <td class="px-6 py-4">
+                        <td class="px-3 py-4">
                             {{ $date(element.created_at) }}
                         </td>
 
-                        <td class="px-6 py-4">
+                        <td class="px-3 py-4">
                             {{ element.codigo_catastro }}
                         </td>
 
@@ -100,29 +100,31 @@ function openPdf(evt) {
                             {{ $numbers.cop(element.total_liquidacion) }}
                         </td>
 
-                        <td class="px-6 py-4">
+                        <td class="px-3 py-4">
                             {{ $date(element.pague_hasta) }}
                         </td>
 
-                        <td class="px-6 py-4">
+                        <td class="px-3 py-4">
                             <StateIndicator :state="element.factura_pagada" enabled="Factura pagada" disabled="Factura sin pagar" />
                         </td>
 
-                        <td class="px-6 py-4">
+                        <td class="px-5 py-4">
                             <div class="flex flex-row justify-between">
                                 <StateIndicator :state="element.state" disabled="Anulado" />
 
                                 <button v-if="element.state && !element.factura_pagada" @click="showModal(element.id)" class="focus:outline-none text-white bg-red-700 focus:ring-4 focus:ring-red-300 font-medium rounded-lg dark:bg-red-600 dark:focus:ring-red-900 hover:bg-red-800 dark:hover:bg-red-700 px-1.5 py-1.5 ms-1">
-                                    <svg class="w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                                    <svg class="w-5 h-5 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
                                         <path fill-rule="evenodd" d="M8.6 2.6A2 2 0 0 1 10 2h4a2 2 0 0 1 2 2v2h3a1 1 0 1 1 0 2v12a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V8a1 1 0 0 1 0-2h3V4c0-.5.2-1 .6-1.4ZM10 6h4V4h-4v2Zm1 4a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Zm4 0a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Z" clip-rule="evenodd"></path>
                                     </svg>
                                 </button>
                             </div>
                         </td>
 
-                        <td class="px-6 py-4">
-                            <a :href="route('factura_predials.show', { factura_predial: element.id })" target="_blank" @click.prevent="openPdf" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
-                                Ver PDF
+                        <td class="px-3 py-4 flex items-center justify-center">
+                            <a :href="route('factura_predials.show', { factura_predial: element.id })" target="_blank" @click.prevent="openPdf" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg px-5 py-2.5 text-center">
+                                <svg class="w-8 h-8" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                                    <path fill-rule="evenodd" d="M9 2.2V7H4.2l.4-.5 3.9-4 .5-.3Zm2-.2v5a2 2 0 0 1-2 2H4a2 2 0 0 0-2 2v7c0 1.1.9 2 2 2 0 1.1.9 2 2 2h12a2 2 0 0 0 2-2 2 2 0 0 0 2-2v-7a2 2 0 0 0-2-2V4a2 2 0 0 0-2-2h-7Zm-6 9a1 1 0 0 0-1 1v5a1 1 0 1 0 2 0v-1h.5a2.5 2.5 0 0 0 0-5H5Zm1.5 3H6v-1h.5a.5.5 0 0 1 0 1Zm4.5-3a1 1 0 0 0-1 1v5c0 .6.4 1 1 1h1.4a2.6 2.6 0 0 0 2.6-2.6v-1.8a2.6 2.6 0 0 0-2.6-2.6H11Zm1 5v-3h.4a.6.6 0 0 1 .6.6v1.8a.6.6 0 0 1-.6.6H12Zm5-5a1 1 0 0 0-1 1v5a1 1 0 1 0 2 0v-1h1a1 1 0 1 0 0-2h-1v-1h1a1 1 0 1 0 0-2h-2Z" clip-rule="evenodd"/>
+                                </svg>
                             </a>
                         </td>
                     </template>
