e6d51bb97c7ff5dc5891c98c77bb133cfaf950f6
EliotVonEcklie <eliotvonecklie@gmail.com>
2024-02-02 09:30:04 -0500

    fix telescope

--- config/telescope.php
+++ config/telescope.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\IdealAuth;
 use Laravel\Telescope\Http\Middleware\Authorize;
 use Laravel\Telescope\Watchers;
 
@@ -77,7 +78,8 @@ return [
 
     'middleware' => [
         'web',
-        Authorize::class,
+        IdealAuth::class,
+        //Authorize::class,
     ],
 
     /*
