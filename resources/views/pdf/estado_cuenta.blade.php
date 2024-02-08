607d54c7acc1fa40a234535e12c4430e15deb347
MrNavySeal <dsparrado197023@gmail.com>
2024-02-08 09:46:45 -0500

    fix estado_cuenta pdf

--- resources/views/pdf/estado_cuenta.blade.php
+++ resources/views/pdf/estado_cuenta.blade.php
@@ -1,10 +1,10 @@
 @php
     $limite = 15;
     $constante = 15;
-    //$vigencias = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,26,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48];
+    //$vigencias = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
     $vigencias = $estadoCuenta->data['liquidacion']['vigencias'];
     $longitud = count($vigencias);
-    $tablas = round($longitud/$limite) > 0 ? round($longitud/$limite) : 1;
+    $tablas = ceil($longitud/$limite) > 0 ? ceil($longitud/$limite) : 1;
     $index = 0;
 
     $estatutoFlags = [
