70047c7d6b0fff46e3940eae8f06f9edb276e939
EliotVonEcklie <eliotvonecklie@gmail.com>
2024-02-22 15:56:16 -0500

    Fix reload button

--- resources/js/Layouts/Private.vue
+++ resources/js/Layouts/Private.vue
@@ -58,7 +58,7 @@ function duplicateWindow() {
 
                     <h1 class="pl-1 text-4xl font-bold text-black dark:text-white">Financiero AMS</h1> <!-- Placeholder -->
                 </div>
-                <div class="grid grid-cols-4 gap-1 sm:gap-3">
+                <div class="grid grid-cols-5 gap-1 sm:gap-3">
                     <button @click="toggleTheme" type="button" class="text-black dark:text-white bg-white dark:bg-black hover:bg-gray-400 hover:black:bg-gray-600 rounded-lg">
                         <svg class="w-6 h-6 mx-auto" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
                             <path v-if="html.classList.contains('dark')" fill-rule="evenodd" d="M13 3a1 1 0 1 0-2 0v2a1 1 0 1 0 2 0V3ZM6.3 5A1 1 0 0 0 5 6.2l1.4 1.5a1 1 0 0 0 1.5-1.5L6.3 5Zm12.8 1.3A1 1 0 0 0 17.7 5l-1.5 1.4a1 1 0 0 0 1.5 1.5L19 6.3ZM12 7a5 5 0 1 0 0 10 5 5 0 0 0 0-10Zm-9 4a1 1 0 1 0 0 2h2a1 1 0 1 0 0-2H3Zm16 0a1 1 0 1 0 0 2h2a1 1 0 1 0 0-2h-2ZM7.8 17.7a1 1 0 1 0-1.5-1.5L5 17.7A1 1 0 1 0 6.3 19l1.5-1.4Zm9.9-1.5a1 1 0 0 0-1.5 1.5l1.5 1.4a1 1 0 0 0 1.4-1.4l-1.4-1.5ZM13 19a1 1 0 1 0-2 0v2a1 1 0 1 0 2 0v-2Z" clip-rule="evenodd"/>
@@ -67,7 +67,7 @@ function duplicateWindow() {
                     </button>
 
                     <button @click="router.reload()" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm py-2 px-4 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                        <svg class="w-6 h-6 mx-auto" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m16 10 3-3m0 0-3-3m3 3H5v3m3 4-3 3m0 0 3 3m-3-3h14v-3"/>
                         </svg>
                     </button>
